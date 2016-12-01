9e56db9021be89cddc70dbb1a8f5dd47951258a3
justinasK <dzastinas@gmail.com>
2016-12-01 08:35:38 +0000

    New parameter page

---
+++ src/main/kotlin/web/demo/controllers/DefautParameters.kt
@@ -0,0 +1,18 @@
+package web.demo.controllers
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import web.demo.model.Person
+
+@RestController
+class DefautParameters {
+    @RequestMapping("/Numbers")
+    fun greeting(@RequestParam(value = "name", defaultValue = "Unknown") name: String,
+                 @RequestParam(value = "age", defaultValue = "20") age: Int
+    ): Person {
+        val jake = Person(name, age, "Stephen's College", "jake.hill@example.com")
+        jake.isEligibleToVote()
+        return jake
+    }
+}
